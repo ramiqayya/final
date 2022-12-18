3ccbda845aca55dbaa91e027133360c4f1448509
ramiqayya <rami.qayya@gmail.com>
2022-12-18 15:07:39 +0200

    import more data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask,redirect, render_template, url_for,request,session
+from flask_session import Session
 from cs50 import SQL
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,8 +7,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///project.db'
-db=SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///project.db'
+# db=SQLAlchemy(app)
+app.config["SESSION_PERMANENT"]=False
+app.config["SESSION_TYPE"]="filesystem"
+Session(app)
+
+
+   
+
 
 @app.route('/')
 def index():
