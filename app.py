00706fd14e33d88107d3037f3056b9916c5ee96e
ramiqayya <rami.qayya@gmail.com>
2022-12-19 10:36:54 +0200

    Many changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask,redirect, render_template, url_for,request,session
 # from flask_session import Session
 # from cs50 import SQL
+from tempfile import mkdtemp
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,24 +10,25 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///project.db'
-# db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///project.db'
+with app.app_context():
+    db=SQLAlchemy(app)
 app.config["TEMPLATES_AUTO_RELOAD"]=True
-app.jinja_env.filters["usd"]=usd
-app.config["SESSION_PERMANENT"]=False
-app.config["SESSION_TYPE"]="filesystem"
-Session(app)
+# app.jinja_env.filters["usd"] = usd
+# app.config["SESSION_PERMANENT"]=False
+# app.config["SESSION_TYPE"]="filesystem"
+# Session(app)
 
-db=SQL("sqlite:///project.db")
+# db=SQL("sqlite:///project.db")
 
-# class Todo(db.Model):
-#     id=db.Coloumn(db.Integer,primary_key=True)
-#     content=db.Column(db.String(200,nullable=False))
-#     completed = db.Column(db.Integer,default=0)
-#     date_created=db.Column(db.DateTime,default=datetime.utcnow)
+class Todo(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(200),nullable=False)
+    completed = db.Column(db.Integer,default=0)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
 
-#     def __repr__(self):
-#        return '<Task %r>' % self.id
+    def __repr__(self):
+       return '<Task %r>' % self.id
 
 
    
