d152d810a870b422f4cdb8a7c7af1f91b8cda646
ramiqayya <rami.qayya@gmail.com>
2022-12-19 14:56:17 +0200

    sitting up new db.. adjust styles making home route

--- app.py
+++ app.py
@@ -34,9 +34,24 @@ class Todo(db.Model):
    
 
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+        task_content=request.form['content']
+        new_task= Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+   
+    
 
 
 if __name__ == "__main__":
